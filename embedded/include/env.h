d5ccb0edf4fad84f7f019e9be019c865b7ae6dc1
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2024-05-18 00:06:03 -0500

    Add files via upload

---
+++ embedded/include/env.h
@@ -0,0 +1,12 @@
+const char* ssid = "MonaConnect";
+const char* password = "";
+String endpoint = "https://project-9fk1.onrender.com";
+#define ONE_WIRE_BUS temp_sensor
+int light_pin = 22;
+// int light_pin = 22;
+int fan_pin = 23;
+int sensorPin = 15;
+int sensorState = LOW;
+int sensorValue = 0;
+
+const int temp_sensor = 4;
